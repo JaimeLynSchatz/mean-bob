9b06500def325547951cd4e04d193abbdd3feccc
JaimeLynSchatz <jaimelynschatz@gmail.com>
2013-10-21 11:13:50 -0700

    changed file name and tweaked bot talk

---
+++ chat.rb
@@ -0,0 +1,28 @@
+puts "..."
+response = gets.chomp()
+puts "Shhh!"
+sleep 1
+puts "Don't say #{response} now! I'm working here."
+sleep 3
+puts "... Fine, sulk. What do you want?"
+response = gets.chomp()
+puts "#{response}!?!? Is that all you have to say?"
+response = gets.chomp()
+response_words = response.split(" ")
+print "Oh, ", response_words[-1], " you say?\n"
+response = gets.chomp()
+puts "Well, why don't you just tell me how you really feel?"
+response = gets.chomp()
+puts "Yeah, yeah, yeah. You didn't think I meant that, did you?"
+response = gets.chomp()
+print "What!?!? ", response, "??? Do you kiss your mother with that mouth?\n"
+puts "I don't think I feel comfortable continuing with this conversation."
+response = gets.chomp()
+puts "I'm waiting for an apology."
+puts "..."
+response = gets.chomp()
+if response.include? 'orry'
+	puts "That's better. Boring conversation anyway. I think I'll take a nap."
+else
+	puts "I've never been so insulted in my life! Goodbye to you!!"
+end
