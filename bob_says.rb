365120101aede28bc160d8f5ea9a01ff2a11f1a0
JaimeLynSchatz <jaimelynschatz@gmail.com>
2013-10-25 11:57:16 -0700

    update bob_says

--- bob_says.rb
+++ bob_says.rb
@@ -1,6 +1,8 @@
-bob_says = [
+bob_says = {
 	"chat" => "Just because I'm called 'chat' doesn't mean I want to!",
 	"nasty" => "Who are YOU calling 'nasty'?!?",
-	"sorry" => "I may accept your apology. I need to think about it."
+	"sorry" => "I may accept your apology. I need to think about it.",
 	"computer" => "Who you callin' 'Computer', chump? This isn't the Enterprise."
-]
+}
+
+puts bob_says["chat"]
