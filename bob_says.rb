f4740ba123a39364d392de28d91ffda3de99d4f2
JaimeLynSchatz <jaimelynschatz@gmail.com>
2013-10-25 11:46:25 -0700

    create bob_says.rb dictionary of known phrases

---
+++ bob_says.rb
@@ -0,0 +1,6 @@
+bob_says = [
+	"chat" => "Just because I'm called 'chat' doesn't mean I want to!",
+	"nasty" => "Who are YOU calling 'nasty'?!?",
+	"sorry" => "I may accept your apology. I need to think about it."
+	"computer" => "Who you callin' 'Computer', chump? This isn't the Enterprise."
+]
