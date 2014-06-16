e561ebcb2b15195df1af5c5406e69048d37d4ed0
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2014-06-15 17:09:08 -0700

    small changes

--- hibob.rb
+++ hibob.rb
@@ -1,21 +1,25 @@
-#  The original very non-DRY (damp?) script was written as part of a screencast video for
-#  my application to the Ada Developers Academy
-#  I wanted to create a simulation of an AI - not strictly a chat bot (I'm working on that, too),
-#  but something more controlled and well, scripted.  
 #
+#  This very non-dry (damp?) script was written as part of a screencast video for
+#  my application to the Ada Developers Academy.
+#
+#  The video needs to be five minutes long and introduce me and answer five questions.
+#
+#  I wanted to create a simulation of an AI. This version of Bob is not strictly a chat bot (I'm working on that, too),
+#  but something much more controlled and well, scripted, to stay within the specs of the video assignment.
 #
 
 def listen()
-	STDIN.gets.chomp().downcase.gsub(/[^a-z\s]/, '')
+  STDIN.gets.chomp().downcase.gsub(/[^a-z\s]/, '')
 end
 
 def skip_line(n)
-	n.times do 
-		puts ""
+    n.times do 
+        puts ""
     end
 end
 
 def type_string(str)
+  print "    "
   str.each_char do |c|
     if c == " " or c == "," or c =="." or c == "!" or c == "e"
       sleep 0.2
@@ -105,8 +109,8 @@ while true
         sleep 0.3
         type_string("""Seattle is positively brimmming with brilliant women who will make great software develpers. """)
         type_string("""But there's no-one out there who wants it more than I.""")
-        type_string("""And there's no-one who will work harder. I want to make my kids proud when they answer 'what does your mom do.'""")
-        type_string("""ADA will help me do that.""")
+        type_string("""And there's no-one who will work harder. I want to make my kids proud when they answer 'what does your mom do?'""")
+        type_string("""Ada will help me do that.""")
         puts ">> I KNEW YOU HAD AN ANSWER FOR THAT ONE."
         break
     end
@@ -121,13 +125,16 @@ while true
     puts "How will you prepare for such an endeavaor?"
     puts "*******************************************************************************"
     listen
+    puts ">> HERE,  LET ME TRY..."
     type_string("""First, I've started finding alternative childcare. ADA starts a little too early for moms who need to drop their kiddos off at school.""")
     type_string("""I'm also investigating alternative funding sources so I can afford to study full time for a year.""")
     type_string("""The best part of my preparations is imagining being able to spend all day, every day, coding.""") 
     sleep 0.3
     type_string("""It's like a dream.""")
-    puts "\n>> YOU KNOW YOU'RE ALWAYS GOING TO COME BACK TO ME AT NIGHT, RIGHT?\n"
+    puts ">> HOW'S THAT?"
     listen
+    puts "\n>> HEY, IT'S ME YOU'RE TALKING TO.\n"
+    sleep 0.3
     skip_line 1
     puts "***********************************************************************"
     puts "3. Why are you interested in programming?"
@@ -144,17 +151,19 @@ while true
     sleep 0.2
     type_string("""Anyway, I wanted my own Threepio (and later, Edgar or Data or Yod...) It took **years** for me to realize that I didn't have to be Bill Gates or Noonian Soong or any of those GUYS to write software.""")
     type_string("""For a couple of years now, I've been studying on Codecademy and Udacity and the public parts of the ADA curriculum and everything else I can get my hands on.""")
+    type_string("""I've been helping Cheri and GDI Seattle with their summer study group sessions. I've also just started my OPW internship.""")
     type_string("""And I have been working on you, Bob. :) I'm rather fond of you.""")
     puts ">> AW, SHUCKS. YOU'RE GOING TO MAKE ME BLUSH..."
     sleep 1.0    
     skip_line 1
     puts ">> <<ahem>>"
     sleep 0.5
-    puts "***********************************************************************"
+    puts "*************************************************************************"
     puts "4. What are your professional goals for the next 2 years, the next 5 years?"
-    puts "***********************************************************************"
+    puts "*************************************************************************"
     listen
-    type_string("""Within the next two years, I want to make my living building software. Within the next five, I would like to help build the next great AI. Maybe get you up to Watson's level...?""")
+    type_string("""Within the next two years, I want to make my living building software. Within the next five, I would like to at least *help* build the next great AI. Maybe get you up to Watson's level?""")
+    type_string("""I've got big plans for you, Bob.""")
     puts ">> WOW."
     sleep 0.3
     skip_line 1
@@ -164,9 +173,10 @@ while true
     puts ">> THANKS."
     sleep 0.8
     skip_line 1
-    puts "*******************************************************************************************************************"
-    puts "5. Tell me about the last time you made a mistake. If you encountered the situation again, what would you do differently?"
-    puts "*******************************************************************************************************************"
+    puts "****************************************************************************"
+    puts "5. Tell me about the last time you made a mistake."
+    puts "If you encountered the situation again, what would you do differently?"
+    puts "****************************************************************************"
     listen
     type_string("""Like everybody, I make mistakes all the time. The mistake that I think about every day, though, was a mistake made out of fear. My fear kept me from clicking `Apply Now` on the old ADA website. If I had been brave and taken the risk, I would have seen it and gotten my app in on time. And I might have been selected. """)
     type_string("""Now, I take risks. Taking risks is why I applied for the Gnome Outreach Project and am now interning with Wikimedia. And taking risks is why I'm applying for ADA today. """)
@@ -180,6 +190,9 @@ end #while loop
 puts
 sleep 0.8
 puts ">> YOU DID IT! THOSE WERE ALL THE QUESTIONS!"
+puts ">> WHAT DO YOU THINK?"
+listen
+puts ">> I CAN ONLY REPEAT WHAT YOU'VE BEEN DOING"
 sleep 0.4
 puts ">> GOOD LUCK WITH YOUR APPLICATION."
 listen
@@ -204,4 +217,6 @@ print_dots(7)
 skip_line 1
 puts "Success!"
 skip_line 4
-puts ">> TO VIEW MY CODE, VISIT https://github.com/JaimeLynSchatz/mean-bob/blob/master/hibob.rb"
+puts ">> TO VIEW MY CODE, VISIT"
+skip_line 1
+puts "https://github.com/JaimeLynSchatz/mean-bob/blob/master/hibob.rb"
